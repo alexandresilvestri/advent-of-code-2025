7a531448366be02036637b159771406efbd798a4
alexandresilvestri <airtontunni@gmail.com>
2025-12-02 13:10:34 -0800

    day 1, start one, complete

--- day1/day1.js
+++ day1/day1.js
@@ -1,21 +1,21 @@
 import fs from 'fs'
 import readline from 'readline'
 
-const fileStream = fs.createReadStream('input.txt');
+const fileStream = fs.createReadStream('input.txt')
 
 const rl = readline.createInterface({
   input: fileStream,
-  crlfDelay: Infinity
-});
+  crlfDelay: Infinity,
+})
 
 let pointer = 50
 let counterZeros = 0
 
 for await (const line of rl) {
-    let direction = line.substring(0, 1)
-    let range = Number(line.substring(1))
+  let direction = line.substring(0, 1)
+  let range = Number(line.substring(1))
 
-    if (direction === 'R') {
+  if (direction === 'R') {
     if (pointer + range <= 99 && range !== 0) {
       pointer = pointer + range
     } else {
@@ -25,24 +25,20 @@ for await (const line of rl) {
         pointer = 0
       }
       pointer = range
-      if (pointer === 0) counterZeros++
     }
   } else {
     if (pointer - range >= 0) {
       pointer = pointer - range
     } else {
-      while (pointer - range < 0 && range !== 0) {
-        const distance = pointer - 1
-        range = range - distance
-        pointer = 99
-        if (range <= 99) {
-          pointer = pointer - range
-          break
+        while (pointer - range < 0 && range !== 0) {
+          const distance = pointer
+          range = range - distance - 1
+          pointer = 99
         }
-      }
+        pointer = pointer - range
     }
-    if (pointer === 0) counterZeros++
   }
+  if (pointer === 0) counterZeros++
 }
 
 console.log(counterZeros)
